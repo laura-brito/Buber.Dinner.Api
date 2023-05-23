4bf2ad4d37490f908d49df8859102938411d6606
laura Brito <laurabritolisboa@gmail.com>
2023-05-23 16:32:43 -0300

    jwt generator, user secrets

---
+++ BuberDinner.Application/Common/Interfaces/Authentication/IJwtTokenGenerator.cs
@@ -0,0 +1,7 @@
+namespace BuberDinner.Application.Common.Interfaces.Authentication;
+
+public interface IJwtTokenGenerator
+{
+    string GenerateToken(Guid userId, string firstName, string lastName);
+
+}
